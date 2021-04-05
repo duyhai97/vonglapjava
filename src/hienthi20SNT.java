2003028ae7a0b76c2a524dd6384ab63b971d3b3f
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-04-05 09:17:20 +0700

    BT

--- src/hienthi20SNT.java
+++ src/hienthi20SNT.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 public class hienthi20SNT {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("nhap so nguyen a: ");
+//        System.out.println("nhap so nguyen a: ");
         long a = s.nextLong();
         int count = 0;
         boolean check = true;
-        int dem = 0;
         if(a<2) count = 0;
         else {
             for (int i = 2; i < a ; i++) {
@@ -22,11 +21,11 @@ public class hienthi20SNT {
 
         for (int i = 0; i < 100; i++) {
             if(check = true){
-                dem++;
+                System.out.println(i);
             }
 
         }
-        System.out.println(dem);
+
         }
 
 
